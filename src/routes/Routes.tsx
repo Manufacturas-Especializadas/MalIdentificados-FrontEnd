58a8f7bfb6dee3fd08d36abbc1675fd1026bc98e
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-07-13 07:45:57 -0600

    feat(route): ruta administrador lineas

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import { Line4 } from "../pages/L-4/Line4";
 import { AdminPanel } from "../pages/Admin/AdminPanel";
+import { AdminLine } from "../pages/Admin/Lines/AdminLine";
 
 export const MyRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Line4 />} />
       <Route path="/administrador" element={<AdminPanel />} />
+      <Route path="/administrador/lineas" element={<AdminLine />} />
     </Routes>
   );
 };
