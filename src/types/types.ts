07f98dbc407df47bff762a51f837ccd6ce6ba462
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-07-13 07:25:13 -0600

    feat(type): type viewState, type Line y type AdminModule

---
+++ src/types/types.ts
@@ -0,0 +1,23 @@
+import type { LucideIcon } from "lucide-react";
+
+export type ViewState =
+  | "dashboard"
+  | "history"
+  | "lines_crud"
+  | "clients_crud"
+  | "parts_crud";
+
+export interface Line {
+  id: number;
+  lineName: string;
+  isActive: boolean;
+  createdAt: string;
+}
+
+export interface AdminModule {
+  id: string;
+  title: string;
+  description: string;
+  icon: LucideIcon;
+  viewTarget: ViewState;
+}
