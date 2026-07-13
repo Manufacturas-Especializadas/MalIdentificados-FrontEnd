9c09f9239da28bcd56a1fd774f2d4aec81767ce1
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-07-13 08:27:33 -0600

    feat(service): metodo - GET

--- src/api/services/LinesService.tsx
+++ src/api/services/LinesService.tsx
@@ -1,3 +1,13 @@
-class LinesService {}
+import { API_CONFIG } from "../../config/api";
+import type { Line } from "../../types/types";
+import { apiClient } from "../client";
+
+class LinesService {
+  private getLinesEndpoint = API_CONFIG.endpoints.lines.getLines;
+
+  async getLines(): Promise<Line[]> {
+    return apiClient.get<Line[]>(this.getLinesEndpoint);
+  }
+}
 
 export const linesService = new LinesService();
