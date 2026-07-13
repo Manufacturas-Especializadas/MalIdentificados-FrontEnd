526841e455a1102c2b9cf4109d414406772d5dee
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-07-13 09:23:03 -0600

    feat(service): metodo - CREATE

--- src/api/services/LinesService.tsx
+++ src/api/services/LinesService.tsx
@@ -4,9 +4,16 @@ import { apiClient } from "../client";
 
 class LinesService {
   private getLinesEndpoint = API_CONFIG.endpoints.lines.getLines;
+  private createEndpoint = API_CONFIG.endpoints.lines.create;
 
   async getLines(): Promise<Line[]> {
     return apiClient.get<Line[]>(this.getLinesEndpoint);
+  }
+
+  async create(lineName: string): Promise<any> {
+    return apiClient.post<any>(this.createEndpoint, {
+      lineName,
+    });
   }
 }
 
