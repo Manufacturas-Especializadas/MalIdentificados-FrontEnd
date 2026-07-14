fb543d020a5fdbcc3346940406585f24ed9dd87f
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-07-14 14:46:58 -0600

    feat(hook): metodo de aprobador de calidad

--- src/hooks/useScanning.ts
+++ src/hooks/useScanning.ts
@@ -9,6 +9,27 @@ export const useScanning = () => {
   const [validationId, setValidationId] = useState<number | null>(null);
   const [isValidating, setIsValidating] = useState<boolean>(false);
 
+  const validateQualityApprover = async (
+    payrollNumber: number,
+  ): Promise<boolean> => {
+    setIsValidating(true);
+    try {
+      const isValid = await scanningServce.validateApprover(payrollNumber);
+
+      if (!isValid) {
+        toast.error("Nómina no autorizada o inactiva en el sistema");
+      }
+
+      return isValid;
+    } catch (error) {
+      console.error("Error al validar auditor", error);
+      toast.error("Hubo un error de conexión al validar la nómina");
+      return false;
+    } finally {
+      setIsValidating(false);
+    }
+  };
+
   const saveCompletedBatch = async (
     payrollNumber: number,
     expectedPartCode: string,
