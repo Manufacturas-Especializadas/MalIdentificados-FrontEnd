0eb4cc090fcc96167021ce723307b06f08ab1926
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-07-14 14:50:11 -0600

    feat(route): ruta microchannel

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -2,11 +2,13 @@ import { Route, Routes } from "react-router-dom";
 import { Line4 } from "../pages/L-4/Line4";
 import { AdminPanel } from "../pages/Admin/AdminPanel";
 import { AdminLine } from "../pages/Admin/Lines/AdminLine";
+import { MicroChannel } from "../pages/MicroChannel/MicroChannel";
 
 export const MyRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Line4 />} />
+      <Route path="/microchannel" element={<MicroChannel />} />
       <Route path="/administrador" element={<AdminPanel />} />
       <Route path="/administrador/lineas" element={<AdminLine />} />
     </Routes>
