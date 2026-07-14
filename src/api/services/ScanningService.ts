b56d8df866072d98d97aab56f29f106d9468d9eb
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-07-14 11:17:41 -0600

    fix(service): utilizamos una type en lugar datos crudos

--- src/api/services/ScanningService.ts
+++ src/api/services/ScanningService.ts
@@ -1,19 +1,12 @@
 import { API_CONFIG } from "../../config/api";
+import type { CompleteBatchPayload } from "../../types/types";
 import { apiClient } from "../client";
 
 class ScanningService {
   private scanningEndpoint = API_CONFIG.endpoints.scanning.start;
 
-  async scanning(
-    payrollNumber: number,
-    partNumberScanned: string,
-    quantity: number,
-  ): Promise<any> {
-    return apiClient.post<any>(this.scanningEndpoint, {
-      payrollNumber,
-      partNumberScanned,
-      quantity,
-    });
+  async saveBatch(payload: CompleteBatchPayload): Promise<any> {
+    return apiClient.post<any>(this.scanningEndpoint, payload);
   }
 }
 
