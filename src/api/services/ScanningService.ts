c310ed79e58c7538cacaf92bda33fcfde30a90cf
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-07-14 08:18:02 -0600

    feat(service): metodo de scaneo

--- src/api/services/ScanningService.ts
+++ src/api/services/ScanningService.ts
@@ -1,3 +1,20 @@
-class ScanningService {}
+import { API_CONFIG } from "../../config/api";
+import { apiClient } from "../client";
+
+class ScanningService {
+  private scanningEndpoint = API_CONFIG.endpoints.scanning.start;
+
+  async scanning(
+    payrollNumber: number,
+    partNumberScanned: string,
+    quantity: number,
+  ): Promise<any> {
+    return apiClient.post<any>(this.scanningEndpoint, {
+      payrollNumber,
+      partNumberScanned,
+      quantity,
+    });
+  }
+}
 
 export const scanningServce = new ScanningService();
